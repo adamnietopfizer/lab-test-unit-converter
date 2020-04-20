0f3e01feb12aae4693e28f1fd398bbb02efc2239
nietoa04 <Adam.Nieto@pfizer.com>
2020-04-20 17:55:41 -0400

    Adds Preprocessor, PubChemAPIWrapper, and SmartConverter classes

---
+++ python-lib/labtestunitconverter/Preprocessor.py
@@ -0,0 +1,11 @@
+"""
+The Preprocessor class is used to determine the majority unit for each lab test.
+"""
+class Preprocessor: 
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
+    
---
+++ python-lib/labtestunitconverter/SmartConverter.py
@@ -0,0 +1,19 @@
+class SmartConverter:
+    
+    def __init__(self):
+        pass
+    
+    def convert(self):
+        pass
+    
+    """
+    Determines if the given row from the dataset is a conversion from SI unit to SI unit or Moles to SI unit.
+    Returns "SI->SI" if SI to SI unit conversion or "Mol->SI" if moles to SI unit conversion
+    """
+    def determine_conversion_type(self):
+        pass
+        
+
+if __name__ == '__main__':
+    pass
+    
